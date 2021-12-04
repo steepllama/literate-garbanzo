b147a8238dc5df08b311397e12a5216737fa358b
steepllama <steepllama@gmail.com>
2021-12-03 22:47:31 -0600

    added timer function

--- main.js
+++ main.js
@@ -41,3 +41,13 @@
  */
 
 
+function timer (){
+    var sec = 300;
+    var timer = setInterval(function() {
+        document.getElementById('timer').innerHTML='00:'+sec;
+        sec--;
+        if (sec < 0) {
+            clearInterval(timer);
+        }
+    }, 1000);
+}
