9b5233381c8982cce086be30ce448df24ca4bfa2
steepllama <steepllama@gmail.com>
2021-12-03 21:50:35 -0600

    incorporated sections for questions

---
+++ main.js
@@ -0,0 +1,43 @@
+
+//create html dynamically 
+
+// when the user click start the timer goes off
+
+//we need to show question and answers ( the quiz is 5 questions with 4 answers each)
+
+
+
+/** TIMER
+ * variable = 60
+ * secondsLeft
+ */
+
+/** questions
+ * create an array of objects
+ * five total objects
+ * 1 question each object
+ * 4 answers in an array
+ * 1 correct answer
+ *
+ *
+ * display question randomly
+ * if correct ans, answer is green
+ * and goes to a different question
+ * until questions are all answered
+ *
+ * else answer is red
+ * loose time
+ * goes to a different question until questions are all answered
+ *
+ *
+ */
+
+/**
+ * Dynamic html
+ *
+ * classes in css
+ * functions to show or display those classes
+ * attach classes to html elements
+ */
+
+
